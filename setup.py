e52b75f3df86420bbc4f6396fa915515027dcd9d
deskent <battenetciz@gmail.com>
2022-06-03 16:31:10 +0300

    0.0.5
    add WalletExists
    delete owner
    delete dir_path
    add docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mycryptowallet-deskent',
-    version='0.0.4',
+    version='0.0.5',
     author='Deskent',
     author_email='battenetciz@gmail.com',
     description='My Crypt Wallet library',
